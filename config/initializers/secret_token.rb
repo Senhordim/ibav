509a2b8d510385d9d9b8fc4f825617d3604c02e8
srdim <srdim@srdim.(none)>
2012-08-18 16:45:50 -0300

    Implementação post

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ibav::Application.config.secret_token = '3e8a323df05b805126fac282a35767fb518e76f7cfd36f0739e6e7ee0e95bd4df196509b78ef4af733028ef3f8558d985e94c6cd9632759e16f78ede142c8ea6'
